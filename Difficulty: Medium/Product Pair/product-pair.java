2c63e1c42d8c108a8d7877efe7937c0a6fbabb18
Nagmani Raj <nagmaniraj133@gmail.com>
2026-05-20 21:38:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Product Pair/product-pair.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isProduct(int[] arr, long target) {
+        HashSet<Long> set = new HashSet<>();
+
+        for (int num : arr) {
+
+            if (target == 0) {
+                if (num == 0 || set.contains(0L)) {
+                    return true;
+                }
+            } else {
+                if (num != 0 && target % num == 0) {
+                    long needed = target / num;
+
+                    if (set.contains(needed)) {
+                        return true;
+                    }
+                }
+            }
+
+            set.add((long) num);
+        }
+
+        return false;
+    }
+}
