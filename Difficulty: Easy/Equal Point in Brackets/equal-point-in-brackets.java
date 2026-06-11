f53830e48ba46199e3b3279fbc63d8ef23b0d6ec
Nagmani Raj <nagmaniraj133@gmail.com>
2026-06-11 06:57:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equal Point in Brackets/equal-point-in-brackets.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findIndex(String s) {
+        int n = s.length();
+
+        int close = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                close++;
+            }
+        }
+
+        int open = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (open == close) {
+                return i;
+            }
+
+            if (s.charAt(i) == '(') {
+                open++;
+            } else {
+                close--;
+            }
+        }
+
+        if (open == close) {
+            return n;
+        }
+
+        return -1;
+    }
+}
