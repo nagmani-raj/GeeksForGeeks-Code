a94ab841cac13701a5deb1795662ab21c78f852b
Nagmani Raj <nagmaniraj133@gmail.com>
2026-07-19 09:55:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Mountain Subarray Queries/mountain-subarray-queries.java
@@ -0,0 +1,35 @@
+class Solution {
+    public ArrayList<Boolean> processQueries(int[] arr, int[][] queries) {
+        int n = arr.length;
+
+        int[] inc = new int[n];
+        inc[n - 1] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] <= arr[i + 1])
+                inc[i] = inc[i + 1];
+            else
+                inc[i] = i;
+        }
+
+        int[] dec = new int[n];
+        dec[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] >= arr[i])
+                dec[i] = dec[i - 1];
+            else
+                dec[i] = i;
+        }
+
+        ArrayList<Boolean> ans = new ArrayList<>();
+
+        for (int[] q : queries) {
+            int l = q[0];
+            int r = q[1];
+
+            int peak = inc[l];
+            ans.add(dec[r] <= peak);
+        }
+
+        return ans;
+    }
+}
